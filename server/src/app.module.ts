2cd1b604874cea8be56c8189a91eb98c1ffee6a4
TuNK0411 <nguyenkhactu991@gmail.com>
2022-07-31 10:06:21 +0700

    update

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { StockController } from './controllers/StockController';
 import { AppService } from './app.service';
+import { StockService } from './services/StockService';
 import AuthModule from 'src/modules/Auth';
 
 @Module({
   imports: [AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, StockController],
+  providers: [AppService, StockService],
 })
 export class AppModule {}
---
+++ server/src/services/StockService.ts
@@ -0,0 +1,56 @@
+import { Model } from 'mongoose';
+import { Injectable, Inject, Req } from '@nestjs/common';
+import ReponseData from 'src/utils/response';
+import { HTTP_STATUS_OK } from 'src/utils/message';
+const fetch = require("node-fetch");
+
+@Injectable()
+export class StockService {
+    constructor(
+        lstItemCode = [
+            {itemCode: '51001', itemName: 'Cao nhất 52 tuần'},
+            {itemCode: '51002', itemName: 'Thấp nhất 52 tuần'},
+            {itemCode: '51003', itemName: 'Vốn hóa thị trường'},
+            {itemCode: '51004', itemName: 'Số CP lưu hành'},
+            {itemCode: '51006', itemName: 'P/E'},
+            {itemCode: '51007', itemName: 'Beta'},
+            {itemCode: '51012', itemName: 'P/B'},
+            {itemCode: '51016', itemName: 'KLGD TB 10 phiên'},
+            {itemCode: '57066', itemName: 'Free float'},
+            {itemCode: '51033', itemName: 'Tỷ suất cổ tức'},
+            {itemCode: '51035', itemName: 'BPVS'},
+        ]
+    ) { }
+
+    async getDataBCTC(code: string) {
+
+        //Infomation company
+        const company_info = await fetch(
+            "https://finfo-api.vndirect.com.vn/v4/company_profiles?q=code:" + code, {
+            headers: {
+                accept: "*/*",
+                "accept-language": "en-US,en;q=0.9,vi;q=0.8",
+                "sec-ch-ua": '" Not A;Brand";v="99", "Chromium";v="101", "Google Chrome";v="101"',
+                "sec-ch-ua-mobile": "?0",
+                "sec-ch-ua-platform": '"Windows"',
+                "host": "finfo-api.vndirect.com.vn",
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "same-origin",
+                "x-requested-with": "XMLHttpRequest",
+                Origin: "https://dstock.vndirect.com.vn",
+                Referer: "https://finfo-api.vndirect.com.vn/",
+                "Referrer-Policy": "strict-origin-when-cross-origin",
+            },
+            method: "GET",
+        }
+        ).then((res) => res.json()
+        ).catch((error) => {
+            console.error("Error data...!");
+            return null;
+          });
+          return new ReponseData(HTTP_STATUS_OK, null, company_info)
+    }
+}
+
+export default StockService;
